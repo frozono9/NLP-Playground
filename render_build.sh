9feeac74d7c62416cb3ef9873b6dd4529bc36f5d
LatorreSony <latorretorras@gmail.com>
2026-02-12 20:33:43 +0100

    feat: Add Render deployment configuration including Procfile, build script for dependencies, NLTK data, and spaCy model downloads.

---
+++ render_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+# Download NLTK data
+python -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words'); nltk.download('wordnet'); nltk.download('sentiwordnet'); nltk.download('omw-1.4')"
+
+# Download spaCy model
+python -m spacy download en_core_web_sm
