e708c17cc75b047400d8f8546dfca55b2f747c0f
LatorreSony <latorretorras@gmail.com>
2026-02-12 22:48:46 +0100

    debug: restore requirements install and verify runtime.txt

--- render_build.sh
+++ render_build.sh
@@ -3,7 +3,10 @@
 set -o errexit
 
 python --version
+echo "Content of runtime.txt:"
+cat runtime.txt
 pip install --upgrade pip
+pip install -r requirements.txt
 
 # Download NLTK data
 python -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words'); nltk.download('wordnet'); nltk.download('sentiwordnet'); nltk.download('omw-1.4')"
