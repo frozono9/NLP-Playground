c34fae4fe5955bd888082d637c36b785db85c501
LatorreSony <latorretorras@gmail.com>
2026-02-12 22:48:21 +0100

    debug: add python version check to build script

--- render_build.sh
+++ render_build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+python --version
+pip install --upgrade pip
 
 # Download NLTK data
 python -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words'); nltk.download('wordnet'); nltk.download('sentiwordnet'); nltk.download('omw-1.4')"
